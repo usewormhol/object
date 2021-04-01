f490c459f52422c73367a70c3c3955c734e52dcd
Ricard Bejarano <ricard@bejarano.network>
2021-04-01 06:00:55 +0200

    added per-file copyright notices

--- random/random.go
+++ random/random.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, Ricard Bejarano. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license, which can be found in the LICENSE file.
+
 package random
 
 import (
